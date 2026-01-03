f2407b3913573f0f757659ebab4afb7d7fa0513f
unknown <Jahnavi.nalamati@gmail.com>
2026-01-03 11:29:27 +0530

    03.01.26

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <h1>Welcome to FEDFUI25 Class</h1>
+        <h2>2500090105-Jahnavi</h2>
+        <h3>03.01.26</h3>
+      </div>
+        
+  )
+}
+
+export default App
